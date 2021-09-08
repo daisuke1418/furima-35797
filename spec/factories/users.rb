e001bd36746467cdda88d7b1f2ea85dc6e362272
daisuke1418 <daisuke.mdv.vbm@icloud.com>
2021-09-08 19:16:55 +0900

    userモデルのテスト

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              { 'daisuke' }
+    email                 { 'test@sample' }
+    password              { 'password1' }
+    password_confirmation { password }
+    first_name            { '大輔' }
+    last_name             { '金津' }
+    first_name_kana       { 'ダイスケ' }
+    last_name_kana        { 'カナズ' }
+    birthday              { '1999-01-01' }
+  end
+end
