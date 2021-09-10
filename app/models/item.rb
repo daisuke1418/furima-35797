c88e1c291c68308a4c5a61b6f16d5a9eb762e687
daisuke1418 <daisuke.mdv.vbm@icloud.com>
2021-09-10 09:31:19 +0900

    画像投稿機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,10 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
+  validates :content, Presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?
+  end
 end
