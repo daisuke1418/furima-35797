0313391e663953e63b9167e2979e9aa65402f6e1
daisuke1418 <daisuke.mdv.vbm@icloud.com>
2021-09-12 14:24:11 +0900

    出品情報をDBへ保存

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,29 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :leadtime
 
-  belongs_to :user
-  has_one_attached :image
-
-  validates :content, Presence: true, unless: :was_attached?
+  with_options numericality: { other_than: 1 , messages: "can't be blank"} do
+    validates :category_id
+    validates :condition_id
+    validates :postage_id
+    validates :prefecture_id
+    validates :lead_time_id
+  end
+  with_options presence: true do
+    validates :title
+    validates :explanation
+    validates :price
+  end
 
   def was_attached?
     self.image.attached?
   end
+
+  belongs_to :user
+  has_one_attached :image
+
 end
